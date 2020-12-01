411689e051a1cc3e38b41b212846becc06f5fac5
roselin <irudayanath@gmail.com>
2020-12-01 12:42:13 +0530

    new commit

---
+++ TestGit/src/Test/Test2.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Test2 {
+public static void main(String args[])
+{
+	System.out.print("test 2");
+	
+	System.out.print("welcome");
+}
+}
---
+++ TestGit/src/Test/Test21.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Test21 {
+public static void main(String args[])
+{
+	System.out.print("Tedt2");
+}
+}
